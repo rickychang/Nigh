371207a9b00ec860c196aabc29e28b70e5eb8649
Ricky Chang <rchang1@gmail.com>
2014-04-08 11:54:00 -0400

    adding Podfile and JSMessagesViewController pod

--- Nigh/NGHAppDelegate.h
+++ Nigh/NGHAppDelegate.h
@@ -7,9 +7,12 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "NGHMultipeerSessionManager.h"
 
 @interface NGHAppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
+
+@property (nonatomic, strong) NGHMultipeerSessionManager *sessionManager;
 
 @end
--- Nigh/NGHChatViewController.m
+++ Nigh/NGHChatViewController.m
@@ -10,6 +10,7 @@
 
 @interface NGHChatViewController ()
 
+
 @end
 
 @implementation NGHChatViewController
